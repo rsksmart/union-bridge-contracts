46525bd5a088ab812ebefb0fd3d489fa32d8594d
Pedro Prete <pmprete@Pedros-MacBook-Pro.local>
2025-02-06 10:29:58 -0300

    Deploy run simulation so no need to re run it

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #https://book.getfoundry.sh/guides/scripting-with-solidity#deploying-our-contract
-bash simulate-deploy.sh
+source .env
+RPC=$RSK_TESTNET_RPC
 echo "================ DEPLOY CONTRACTS TO $RPC ================"
 forge script \
-    $SCRIPT \
+    script/Deploy.s.sol \
     --rpc-url $RPC \
     --legacy \
     --broadcast \
--- simulate-deploy.sh
+++ simulate-deploy.sh
@@ -4,9 +4,7 @@ echo "================ SIMULATE DEPLOY ================"
 rm -rf broadcast/Deploy.s.sol/31/dry-run
 rm -rf cache/Deploy.s.sol/31/dry-run
 source .env
-SCRIPT=script/Deploy.s.sol
-RPC=$RSK_TESTNET_RPC
-forge script $SCRIPT \
-    --rpc-url $RPC \
+forge script script/Deploy.s.sol \
+    --rpc-url $RSK_TESTNET_RPC \
     --legacy \
     -vvv
