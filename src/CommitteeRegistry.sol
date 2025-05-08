bd543941eab53f86417fb3811418e750269521dd
momiagitana <64145207+momiagitana@users.noreply.github.com>
2025-05-08 08:58:19 -0300

    delete non implemented feature flag, batchFlag from requestPegOut function

--- src/CommitteeRegistry.sol
+++ src/CommitteeRegistry.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.20;
 
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {BaseProxy} from "./BaseProxy.sol";
--- src/SecurityBond.sol
+++ src/SecurityBond.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.20;
 
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {IStreamManager} from "./interfaces/IStreamManager.sol";
 
 abstract contract SecurityBond {
