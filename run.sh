6ca7f4f4506e2bc82e055ad39ed3b3bc9cfe0dc6
Pedro Prete <pmprete@gmail.com>
2025-06-24 16:08:37 -0300

    Delete local-packet-creation-full-flow move integration test scripts to own folder Packet creation flow is running also in the peg flow maybe it's not needed here Also refactor scripts that are used as integration tests to their own folder

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ set -eux
 bash shell/script/deploy/simulate-deploy.sh
 # gas report also runs test
 bash shell/gas-report.sh
-# packet creation flow is running also in the peg flow maybe it's not needed here
-bash shell/script/local-packet-creation-full-flow.sh 
-bash shell/script/local-peg-full-flow.sh
+# packet creation flow is running in the peg flow
+bash shell/script/integration-test/local-peg-full-flow.sh
 bash shell/slither.sh
