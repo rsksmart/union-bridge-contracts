36a94a3b561c230bce4a012f905fbee2811f4343
Sebastian Grunberg <sebastian.grunberg@fairgate.io>
2025-11-14 15:51:35 +0000

    Add a check for the pending committee expired when depositing communication data

--- src/CommitteeRegistry.sol
+++ src/CommitteeRegistry.sol
@@ -304,17 +304,6 @@ contract CommitteeRegistry is ICommitteeRegistry, AccessControl, ReentrancyGuard
             }
         }
 
-        // Verify that the status of the pending committee has not changed to expired.
-        // slither-disable-next-line timestamp
-        if (block.timestamp >= pendingCommittee.createdAt + pendingCommitteeTimeout) {
-            revert PendingCommitteeExpired(
-                _committeeId,
-                block.timestamp,
-                pendingCommittee.createdAt,
-                pendingCommittee.createdAt + pendingCommitteeTimeout
-            );
-        }
-
         pendingCommittee.missingData--;
         emit MemberInfoDeposited(_committeeId, sender, _aggregatedKey);
         if (pendingCommittee.missingData != 0) {
