566c002205390254cb8a1ee559ae069d6c33964f
Pedro Prete <pmprete@gmail.com>
2026-01-13 13:25:17 -0300

    Add reject-pegin shell script to run.sh and ci

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 set -eux
+# simulate deploy against rsk testnet
 bash shell/script/deploy/simulate-deploy.sh
-# gas report also runs test
+# gas report (Is also runs tests)
 bash shell/gas-report.sh
 # packet creation flow is running in the peg flow
 bash shell/script/integration-test/local-peg-full-flow.sh
+# user reimbursement flow
 bash shell/script/integration-test/user-reimbursement-flow.sh
+# reject pegin flow
+bash shell/script/integration-test/reject-pegin-flow.sh
+# run slither
 bash shell/slither.sh
