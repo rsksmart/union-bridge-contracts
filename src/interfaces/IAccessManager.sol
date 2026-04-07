56ce449ec51ac6aa9dd4870362e0a16733d9dd2a
Marcos <marcos@rootstocklabs.com>
2026-04-07 14:23:06 -0300

    Update License in source files to MIT

--- src/interfaces/IAccessManager.sol
+++ src/interfaces/IAccessManager.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 /// @notice Interface for access control in the union bridge
--- test/helpers/OperatorTakeManagerHarness.sol
+++ test/helpers/OperatorTakeManagerHarness.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import {OperatorTakeManager} from "src/OperatorTakeManager.sol";
